436236b7d7fb662a0e8332c8474e33a4a497c762
Ciro Goyeneche <soycirog@gmail.com>
2021-04-17 20:06:17 -0300

    Handling FileNotFound exception

--- utils.py
+++ utils.py
@@ -1,14 +1,18 @@
 """
 This file contains utils functions
 """
+from flask import abort
 
 def get_quotes_from_file(filename):
     '''
         Return a list of quotes from a given file. 
     '''
     quotes = []
-    with open(filename, "r") as quotes_file:
-        for quote in quotes_file:
-            quote = quote.replace('\n', '')
-            quotes.append(quote)
-    return quotes
+    try:
+        with open(filename, "r") as quotes_file:
+            for quote in quotes_file:
+                quote = quote.replace('\n', '')
+                quotes.append(quote)
+        return quotes
+    except FileNotFoundError:
+        abort(500, "File not found")
