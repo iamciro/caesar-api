a6527f597c6924836e63610dc3180fe3e1d0ba51
Ciro Goyeneche <soycirog@gmail.com>
2021-04-17 16:05:39 -0300

    index route added

--- api.py
+++ api.py
@@ -5,13 +5,22 @@ This API generates quotes from the very famous Roman emperor, Caesar.
 Author: Ciro Goyeneche
 """
 import dotenv
-
-from flask import Flask
+from flask import Flask, jsonify
 
 # Load .env config
 dotenv.load_dotenv()
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    info = {
+        "message": "Welcome to Caesar API",
+        "actions": [
+            "GET /quote"
+        ]
+    }
+    return jsonify(info)
 
 if __name__ == "__main__":
     app.run()
